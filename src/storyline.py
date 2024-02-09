08039fd72f3a2c12b9f3bbca332a962e0e98d968
Ozurk <157186017+Ozurk@users.noreply.github.com>
2024-02-09 14:51:28 -0500

    added in the whole map
    the map is still empty for the most part
    set it up with 7 interconnected locations and tasks
    
    made terminal size a variable
    removed some of the old stuff
    
    made random events part of the supervisor function
    
    made the supervisor function part off all of the locations and tasks
    
    made a 1 through 3 function
    
    thats about it

--- src/storyline.py
+++ src/storyline.py
@@ -1,24 +1,31 @@
 import logging
 import os
 from random import randint
+import shutil
 from functions import *
 from random import randint
 import pyautogui
+terminal_width = shutil.get_terminal_size().columns
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-sleep_time = 0.2
-stats = {"money": 1, "sanity": 100, "food": 100}
+sleep_time = 0.00
+stats = {"money": 10000, "sanity": 100, "food": 100}
 inventory = {}
-player_type = "[blank]"
+player_variable = "[blank]"
 
 
+# ----------------------------------------------------------------------------------------------------------------------
 def supervisor():
+    os.system("cls")
     user_stats()
     if stats["food"] <= 0:
         starvation()
     if stats["sanity"] <= 0:
-        print('you lost your mind')
+        insane()
+    if stats['money'] <= 0:
+        beg()
+    if randint(1, 5) == 1:
+        random_event()
 
 
 def insane():
@@ -37,53 +44,55 @@ def insane():
             print("You are trying to close the instance of your game")
             counter += 1
     input("There was a cool way the game was suppose to work, but it did not work properly...\n press enter to quit")
-    quit("goodbye...")
-
-
-insane()
+    death()
 
 
 def starvation():
     os.system("cls")
-    print("-" * 150)
-    print("-" * 150)
-    print("It appears you have no food...".center(150, " "))
+    print("-" * terminal_width)
+    print("-" * terminal_width)
+    print("It appears you have no food...".center(terminal_width, " "))
     if stats['money'] < 1000:
         beg()
-    print("-" * 150)
-    print("-" * 150)
-    print('you can roll the dice begging for food [1]'.center(150, " "))
-    print("-" * 150)
-    if stats["money"] > 1000:
-        print("or you can visit the vending machine to buy some more food [2]".center(150, " "))
-    print("-" * 150)
-    print("-" * 150)
-    one_or_two()
-    print("-" * 150)
+    print("-" * terminal_width)
+    print("-" * terminal_width)
+    print('you can press your luck begging for money [1]'.center(terminal_width, " "))
+    print("-" * terminal_width)
+    if stats["money"] > 100:
+        print("or you can visit the vending machine to buy some more food [2]".center(terminal_width, " "))
+    print("-" * terminal_width)
+    print("-" * terminal_width)
+    user_picks = one_or_two()
+    if user_picks == 1:
+        beg()
+    elif user_picks == 2:
+        vending_machine()
+    else:
+        print("Please report this to Hunter if you see this...Something has malfunctioned...")
+    print("-" * terminal_width)
 
 
 def vending_machine():
     os.system("cls")
     print("welcome to the vending machine!\n")
     print("you have: " + str(stats["money"]) + " PD\n\n")
-    print("1000 PD for 10 food\n")
+    print("100 PD for 10 food\n")
     food_quantity = stats["money"] // 1000
     print("you can buy up to: [" + str(food_quantity) + ']\n')
-    food_number = number_regex(input("enter the the amount of food you want to purchase".center(150, '-') + "\n"))
-    stats["money"] -= (food_number * 1000)
-    stats["food"] += (food_number * 10)
+    food_number = number_regex(input("enter the the amount of food you want to purchase".center(terminal_width, '-') + "\n"))
+    stats["money"] -= (food_number * 100)
+    stats["food"] += (food_number * 1)
     time.sleep(2)
-    print('thank you for visiting :)'.center(150, ' '))
-    supervisor()
+    print('thank you for visiting :)'.center(terminal_width, ' '))
 
 
 def beg():
-    print("-" * 115)
-    print("-" * 115)
+    print("-" * terminal_width)
+    print("-" * terminal_width)
     print("You ran out of food and money, and you are loosing your sanity quickly...")
     input("you are sitting outside of the rest-stop with a sign begging for money.")
-    print("-" * 115)
-    while stats["money"] < 1000:
+    print("-" * 175)
+    while stats["money"] < 100:
         spinner(randint(1, 5))
         d10_die = randint(1, 30)
         if d10_die == 1:
@@ -92,7 +101,7 @@ def beg():
             vending_machine()
         elif d10_die == 10:
             input("The stranger killed you")
-            # todo death
+            death()
         elif 11 < d10_die < 20:
             donation = randint(1, 300)
             print('The stranger gave you ' + str(donation) + " PD")
@@ -107,81 +116,34 @@ def beg():
             print("you have " + str(stats["money"]) + " PD")
             print("your sanity is at " + str(stats["sanity"]) + "%")
         if stats["sanity"] < 0:
-            print("you lost your mind...")
-            # todo death
+            insane()
     vending_machine()
-
-
-def intro_screen():
-    # this is the fancy little intro screen
-    typing_effect("-" * 115, .005)
-    print()
-    typing_effect("-" * 115, .005)
-    print()
-    typing_effect("-" * 115, .005)
-    typing_effect("""  
-             #####  #######    #    ######   #####     #    ### #       ####### ######  
-            #     #    #      # #   #     # #     #   # #    #  #       #     # #     # 
-            #          #     #   #  #     # #        #   #   #  #       #     # #     # 
-             #####     #    #     # ######   #####  #     #  #  #       #     # ######  
-                  #    #    ####### #   #         # #######  #  #       #     # #   #   
-            #     #    #    #     # #    #  #     # #     #  #  #       #     # #    #  
-             #####     #    #     # #     #  #####  #     # ### ####### ####### #     # """, .005)
-    print("\n")
-    typing_effect("-" * 115, .005)
-    print()
-    typing_effect("-" * 115, .005)
-    print()
-    typing_effect("-" * 115, .005)
-    time.sleep(.75)
-    print("\n" * 8)
-    input("Press Enter to Continue...".center(115, " ") + "\n")
-    print("\n")
 
 
 def user_stats():
     items_list = ["food", 'money', 'sanity']
     iteration = 0
-    print("-" * 115)
-    print(('You are playing as: ' + player_type).center(115, " "))
+    print("-" * 175)
+    print(('You are playing as: ' + player_variable).center(terminal_width, " "))
     for items in items_list:
-        print("-" * 115)
-        print(((items_list[iteration]) + ": " + str(stats[items_list[iteration]])).center(115, " "))
-        print("-" * 115)
+        print("-" * 175)
+        print(((items_list[iteration]) + ": " + str(stats[items_list[iteration]])).center(terminal_width, " "))
+        print("-" * 175)
         iteration += 1
     print("\n\n")
-    input("press enter to contiue".center(115, "-") + "\n")
 
 
 def intro():
     os.system("cls")
     typing_effect(get_file(r".\storyline\intro"), .005)
     print("\n" * 4)
-    input("Press enter to play the game".center(115, "-") + "\n")
-    user_stats()
+    input("Press enter to play the game".center(terminal_width, "-") + "\n")
     set_and_setting()
 
 
 def set_and_setting():
-    global player_type
     os.system("cls")
-    typing_effect(get_file(r"storyline\chapter_1\setting and setup"), .005)
-    player_type_options = ['1', '2', '3', '4']
-    # make this a dictionary
-    player_type = input("please enter one of the following:\n[1] sailor\n[2] scout\n[3] smuggler\n[4] scavver\n")
-    while player_type not in player_type_options:
-        player_type = input("please enter one of the following:\n[1] sailor\n[2] scout\n[3] smuggler\n[4] scavver\n")
-    logging.debug("the player type is %s", player_type)
-    player_type = player_type.lower().strip()
-    if player_type == "1":
-        player_type = 'sailor'
-    elif player_type == "2":
-        player_type = 'scout'
-    elif player_type == "3":
-        player_type = "smuggler"
-    elif player_type == "4":
-        player_type = 'scavver'
-    user_stats()
+    typing_effect(get_file(r"storyline\setup\setting and setup"), .005)
     tunnel()
 
 
@@ -191,31 +153,10 @@ def tunnel():
     The Smokestacks are the colloquially named shanty-apartments residing above 
     The Foundry, the city's blazing furnace of manufactury. The choking smog is a perennial feat
     ure of your lodgings, but doesn't it seem particularly thick this evening?\n\n""", .000)
-    input("Press enter to continue".center(115) + '\n')
-    typing_effect(get_file(r"storyline//chapter_1/tunnel"), .000)
-    tunnel_options = {"1": "option1", "2": "option2", "3": "option3"}
-    tunnel_choice = input("\n")
-    while tunnel_choice not in tunnel_options.keys():
-        tunnel_choice = input("Please enter a\n[1]\n[2]\n[3]\n")
-    if tunnel_choice == tunnel_options["1"]:
-        print("option 1 ")
-        # todo entrance_option_1()
-    elif tunnel_choice == tunnel_options["2"]:
-        print("Option 2")
-        # todo entrance_option_2()
-    elif tunnel_choice == tunnel_options["3"]:
-        print("option 3")
-        # todo entrance_option_3
+    input("Press enter to continue".center(terminal_width) + '\n')
+    typing_effect(get_file(r"storyline/setup/tunnel"), .000)
+    location_1()
 
-
-# def entrance_option_1():
-# todo
-
-# def entrance_option_2():
-# todo
-
-# def entrance_option_1():
-# todo
 
 def random_event():
     os.system("cls")
@@ -233,32 +174,31 @@ def jury_duty():
             Despite all efforts, you can not wiggle out of this one...\n
             You lose a little bit of your mind...''', 0)
     print('\n')
-    print("-" * 115)
-    input('Press Enter to Continue'.center(115))
+    print("-" * 175)
+    input('Press Enter to Continue'.center(terminal_width))
     os.system("cls")
     stats["sanity"] -= 10
     stats["money"] += 10
-    user_stats()
 
 
 def crypto():
     typing_effect("You hit it big in the crypto^3 market despite having no idea "
-                  "what you are doing.".center(115), 0)
+                  "what you are doing.".center(terminal_width), 0)
     print("\n")
-    typing_effect("+ 2000 platinum disks".center(115), 0)
+    typing_effect("+ 1750 platinum disks".center(terminal_width), 0)
     print('\n')
-    print("-" * 115)
+    print("-" * 175)
     print("\n")
-    input('Press Enter to Continue'.center(115))
+    input('Press Enter to Continue'.center(terminal_width))
     stats["money"] += 1000
     os.system("cls")
-    user_stats()
 
 
 def broken_down():
-    typing_effect("There is a ship broken-down along the side of Desmos 9, do you want to help it?".center(115), 0)
+    typing_effect("There is a ship broken-down along the side of Desmos 9, do you wan"
+                  "t to help it?".center(terminal_width), 0)
     print('\n')
-    print("-" * 115)
+    print("-" * 175)
     user_choose = yes_or_no("\ny[yes]\nor[no]\n")
     if user_choose.lower().strip() == "yes":
         dice_roll = randint(1, 6)
@@ -267,26 +207,199 @@ def broken_down():
             logging.debug("the number 6 option is running")
             typing_effect("The ship was very grateful.\nAll they needed was a quick jump.", 0)
             typing_effect("They rewarded you handsomely...\n + 5000 platinum disks\n", 0)
-            print("-" * 115)
-            input('Press Enter to Continue'.center(115) + "\n")
+            print("-" * 175)
+            input('Press Enter to Continue'.center(terminal_width) + "\n")
             stats["money"] += 5000
-            user_stats()
             os.system("cls")
         elif dice_roll == 1:
             typing_effect("The ship was full of pirates. It was a trap, they took half of everything\n", 0)
-            print("-" * 115)
-            input('Press Enter to Continue'.center(115))
+            print("-" * 175)
+            input('Press Enter to Continue'.center(terminal_width))
             os.system('cls')
             stats["food"] *= .5
             stats["sanity"] *= .5
             stats["money"] *= .5
-            user_stats()
         else:
             typing_effect("""The ship did not want your help..\n you continue on...\n""", 0)
-            print("-" * 115)
-            user_stats()
-    elif user_choose.lower().strip() == "no":
-        user_stats()
+            print("-" * 175)
 
 
-beg()
+def death():
+    typing_effect(get_file(r"storyline\endgame\death"), sleep_time)
+    input("Press Enter to quit...".center(terminal_width, "-"))
+    quit()
+    # todo add a better death screen
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------location 1-----------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def task_1():
+    supervisor()
+    print("this is task 1")
+    input("return to location_1")
+    # todo rename this
+    location_1()
+
+
+def location_1():
+    supervisor()
+    typing_effect(get_file(r"storyline/location_1/location1"), sleep_time)
+    choice = one_through_3()
+    if choice == 1:
+        task_1()
+    elif choice == 2:
+        location_7()
+    elif choice == 3:
+        location_2()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------location 2-----------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def task_2():
+    supervisor()
+    print("this is task 2")
+    input('return to location 2')
+    task_2()
+
+
+def location_2():
+    supervisor()
+    typing_effect(get_file(r"storyline/location_2/location2"), sleep_time)
+    choice = one_through_3()
+    if choice == 1:
+        task_2()
+    elif choice == 2:
+        location_3()
+    elif choice == 3:
+        location_1()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------location 3-----------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def task_3():
+    supervisor()
+    print("this is task 3")
+    input('return to location 3')
+    location_3()
+
+
+def location_3():
+    supervisor()
+    typing_effect(get_file(r"storyline/location_3/location3"), sleep_time)
+    choice = one_through_3()
+    if choice == 1:
+        task_3()
+    elif choice == 2:
+        location_4()
+    elif choice == 3:
+        location_2()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------location 4-----------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def task_4():
+    supervisor()
+    print("this is task 4")
+    input('return to location 4')
+    location_4()
+
+
+def location_4():
+    supervisor()
+    typing_effect(get_file(r"storyline/location_4/location4"), sleep_time)
+    choice = one_through_3()
+    if choice == 1:
+        task_4()
+    elif choice == 2:
+        location_5()
+    elif choice == 3:
+        location_3()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------location 5-----------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def task_5():
+    supervisor()
+    print("this is task 5")
+    input('return to location 5')
+    location_5()
+
+
+def location_5():
+    supervisor()
+    typing_effect(get_file(r"storyline/location_5/location5"), sleep_time)
+    choice = one_through_3()
+    if choice == 1:
+        task_5()
+    elif choice == 2:
+        location_6()
+    elif choice == 3:
+        location_4()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------location 6-----------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def task_6():
+    supervisor()
+    print("this is task 5")
+    input('return to location 5')
+    location_6()
+
+
+def location_6():
+    supervisor()
+    typing_effect(get_file(r"storyline/location_6/location6"), sleep_time)
+    choice = one_through_3()
+    if choice == 1:
+        task_6()
+    elif choice == 2:
+        location_7()
+    elif choice == 3:
+        location_5()
+
+
+# ---------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------location 6-----------------------------------------------------------
+# ---------------------------------------------------------------------------------------------------------------------
+
+
+def task_7():
+    supervisor()
+    print("this is task 7")
+    input('return to location 7')
+    location_7()
+
+
+def location_7():
+    supervisor()
+    typing_effect(get_file(r"storyline/location_7/location7"), sleep_time)
+    choice = one_through_3()
+    if choice == 1:
+        task_7()
+    elif choice == 2:
+        location_1()
+    elif choice == 3:
+        location_6()
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+location_1()
