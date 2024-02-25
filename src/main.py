5cf794986952c17f8e5305d53db06574ec4c2271
Ozurk <157186017+Ozurk@users.noreply.github.com>
2024-02-25 07:57:54 -0500

    refactored stotyline.py
    rem's function is now working
    made the  user attributes a class instead of global variables

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
 import logging
-import os
-from random import randint
-import shutil
 from functions import *
 from random import randint
 import pyautogui
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 sleep_time = 0.00
-stats = {"money": 10000, "sanity": 100.00, "food": 100.00}
-inventory = {}
-player_variable = "[blank]"
+
+
+class Player:
+    stats = {"money": 10000, "sanity": 100.00, "food": 100.00}
+    inventory = {}
+    player_variable = "[blank]"
+
 
 terminal_width = shutil.get_terminal_size().columns
 
@@ -19,14 +20,14 @@ terminal_width = shutil.get_terminal_size().columns
 def supervisor():
     os.system("cls")
     user_stats()
-    if stats["food"] <= 0:
+    if Player.stats["food"] <= 0:
         starvation()
-    if stats["sanity"] <= 0:
+    if Player.stats["sanity"] <= 0:
         insane()
-    if stats['money'] <= 0:
+    if Player.stats['money'] <= 0:
         beg()
-    if randint(1, 5) == 1:
-        random_event()
+    # if randint(1, 5) == 1:
+    # random_event()
     input("Press enter to continue...".center(terminal_width) + "\n")
 
 
@@ -54,13 +55,13 @@ def starvation():
     print("-" * terminal_width)
     print("-" * terminal_width)
     print("It appears you have no food...".center(terminal_width, " "))
-    if stats['money'] < 1000:
+    if Player.stats['money'] < 1000:
         beg()
     print("-" * terminal_width)
     print("-" * terminal_width)
     print('you can press your luck begging for money [1]'.center(terminal_width, " "))
     print("-" * terminal_width)
-    if stats["money"] > 100:
+    if Player.stats["money"] > 100:
         print("or you can visit the vending machine to buy some more food [2]".center(terminal_width, " "))
     print("-" * terminal_width)
     print("-" * terminal_width)
@@ -77,14 +78,14 @@ def starvation():
 def vending_machine():
     os.system("cls")
     print("welcome to the vending machine!\n")
-    print("you have: " + str(stats["money"]) + " PD\n\n")
+    print("you have: " + str(Player.stats["money"]) + " PD\n\n")
     print("100 PD for 10 food\n")
-    food_quantity = stats["money"] // 1000
+    food_quantity = Player.stats["money"] // 1000
     print("you can buy up to: [" + str(food_quantity) + ']\n')
     food_number = number_regex(
         input("enter the the amount of food you want to purchase".center(terminal_width, '-') + "\n"))
-    stats["money"] -= (food_number * 100)
-    stats["food"] += (food_number * 1)
+    Player.stats["money"] -= (food_number * 100)
+    Player.stats["food"] += (food_number * 1)
     time.sleep(2)
     print('thank you for visiting :)'.center(terminal_width, ' '))
 
@@ -95,12 +96,12 @@ def beg():
     print("You ran out of food and money, and you are loosing your sanity quickly...")
     input("you are sitting outside of the rest-stop with a sign begging for money.")
     print("-" * terminal_width)
-    while stats["money"] < 100:
+    while Player.stats["money"] < 100:
         spinner(randint(1, 5))
         d10_die = randint(1, 30)
         if d10_die == 1:
             input("The stranger was kind and gave you 1000 PD, you can now visit the vending machine.")
-            stats["money"] += 1000
+            Player.stats["money"] += 1000
             vending_machine()
         elif d10_die == 10:
             input("The stranger killed you")
@@ -108,17 +109,17 @@ def beg():
         elif 11 < d10_die < 20:
             donation = randint(1, 300)
             print('The stranger gave you ' + str(donation) + " PD")
-            stats['money'] += donation
-            if stats["sanity"] < 100:
-                stats["sanity"] += 1
-            print("you have " + str(stats["money"]) + " PD")
-            print("your sanity is at " + str(stats["sanity"]) + "%")
+            Player.stats['money'] += donation
+            if Player.stats["sanity"] < 100:
+                Player.stats["sanity"] += 1
+            print("you have " + str(Player.stats["money"]) + " PD")
+            print("your sanity is at " + str(Player.stats["sanity"]) + "%")
         else:
             print("The stranger passed by, pretending not to notice you")
-            stats["sanity"] -= 5
-            print("you have " + str(stats["money"]) + " PD")
-            print("your sanity is at " + str(stats["sanity"]) + "%")
-        if stats["sanity"] < 0:
+            Player.stats["sanity"] -= 5
+            print("you have " + str(Player.stats["money"]) + " PD")
+            print("your sanity is at " + str(Player.stats["sanity"]) + "%")
+        if Player.stats["sanity"] < 0:
             insane()
     vending_machine()
 
@@ -127,13 +128,13 @@ def user_stats():
     items_list = ["food", 'money', 'sanity']
     iteration = 0
     print("-" * terminal_width)
-    print(('You are playing as: ' + player_variable).center(terminal_width, " "))
+    print(('You are playing as: ' + Player.player_variable).center(terminal_width, " "))
     for items in items_list:
         print("-" * terminal_width)
-        print(((items_list[iteration]) + ": " + str(stats[items_list[iteration]])).center(terminal_width, " "))
+        print(((items_list[iteration]) + ": " + str(Player.stats[items_list[iteration]])).center(terminal_width, " "))
         print("-" * terminal_width)
         iteration += 1
-    print(("Inventory: " + str(inventory)).center(terminal_width))
+    print(("Player.inventory: " + str(Player.inventory)).center(terminal_width))
     print("\n\n")
 
 
@@ -162,57 +163,65 @@ def tunnel():
 
 
 def ssin_validtator():
-    supervisor()
-    ssin_input = input("Please Enter 16 digits. No spaces or dashes\n[****-****-****-****]\n")
-    ssin_success = even_and_odd_validation(number_validation(length_validation(ssin_input.strip())))
-    inventory["ssid"] = ssin_success
-    typing_effect("\n Thank you for visiting the Flagstaff BMV\n Have a nice day :)", sleep_time)
     os.system("cls")
+    ssin = input("Please Enter 16 digits.\n").strip()
+    even_and_odd_validation(number_validation(length_validation(ssin)))
+    Player.inventory["ssid"] = even_and_odd_validation(number_validation(length_validation(ssin)))
+    input("Thank you for visiting Flagstaff AZ BMV\nHave a great day")
 
 
 def number_validation(length_validation_input):
-    if not length_validation_input.isalnum():
-        input("This is not a valid input. Try Again\n #VALUE\n")
-        stats["sanity"] *= .99
-        ssin_validtator()
-    else:
+    if length_validation_input.isdigit():
         return length_validation_input
+    else:
+        print("\nThe value you entered is invalid\n #VALUE")
+        time.sleep(3)
+        ssin_validtator()
+        pass
 
 
 def length_validation(ssin):
-    if len(ssin) != 16:
-        print("This entry is invalid\nError 411\n")
-        input("press enter to try again\n")
-        stats["sanity"] *= .99
-        ssin_validtator()
-    else:
-        return ssin
+    try:
+        if len(ssin) != 16:
+            print("This entry is invalid\nError 411\n")
+            time.sleep(3)
+            Player.stats["sanity"] *= .99
+            ssin_validtator()
+            pass
+        else:
+            logging.debug(" len validator ssin is %s", ssin)
+            return ssin
+    except AttributeError:
+        pass
 
 
 def even_and_odd_validation(number):
     odd_numbers = number[::2]
     for digits in odd_numbers:
-        # TODO fix the number validator
         if float(digits) % 2 == 0:
-            stats["sanity"] -= .5
+            Player.stats["sanity"] -= .5
             print("The number you entered was not valid.\n"
-                  "The number that caused an error was: " + digits)
-            input("\n[" + number + "]")
-            stats["sanity"] *= .99
-            input("\nYou lost a little bit of sanity...")
-
+                  "The number that caused an error was: " + digits + "\n")
+            print("\n[" + number + "]")
+            time.sleep(3)
+            Player.stats["sanity"] *= .99
+            print("\nYou lost a little bit of sanity...")
+            time.sleep(3)
             ssin_validtator()
-
+            pass
     even_numbers = number[1::2]
     for even_digits in even_numbers:
         if float(even_digits) % 2 != 0:
             print("The number you entered was not valid.\n"
-                  "The number that caused an error was: " + even_digits)
-            input("\n[" + number + "]")
-            stats["sanity"] *= .99
+                  "The number that caused an error was: " + even_digits + "\n")
+            time.sleep(3)
+            print("\n[" + number + "]")
+            time.sleep(3)
+            Player.stats["sanity"] *= .99
             print("\nYou lost a little bit of sanity")
             ssin_validtator()
-
+            pass
+    logging.debug("the even/odd validator return value is %s", number)
     return number
 
 
@@ -264,8 +273,8 @@ def jury_duty():
     print("-" * terminal_width)
     input('Press Enter to Continue'.center(terminal_width))
     os.system("cls")
-    stats["sanity"] -= 10
-    stats["money"] += 10
+    Player.stats["sanity"] -= 10
+    Player.stats["money"] += 10
     supervisor()
 
 
@@ -278,7 +287,7 @@ def crypto():
     print("-" * terminal_width)
     print("\n")
     input('Press Enter to Continue'.center(terminal_width))
-    stats["money"] += 1000
+    Player.stats["money"] += 1000
     os.system("cls")
     supervisor()
 
@@ -298,16 +307,16 @@ def broken_down():
             typing_effect("They rewarded you handsomely...\n + 5000 platinum disks\n", 0)
             print("-" * terminal_width)
             input('Press Enter to Continue'.center(terminal_width) + "\n")
-            stats["money"] += 5000
+            Player.stats["money"] += 5000
             os.system("cls")
         elif dice_roll == 1:
             typing_effect("The ship was full of pirates. It was a trap, they took half of everything\n", 0)
             print("-" * terminal_width)
             input('Press Enter to Continue'.center(terminal_width))
             os.system('cls')
-            stats["food"] *= .5
-            stats["sanity"] *= .5
-            stats["money"] *= .5
+            Player.stats["food"] *= .5
+            Player.stats["sanity"] *= .5
+            Player.stats["money"] *= .5
         else:
             typing_effect("""The ship did not want your help..\n you continue on...\n""", 0)
             print("-" * terminal_width)
@@ -493,4 +502,4 @@ def location_7():
 
 # ----------------------------------------------------------------------------------------------------------------------
 
-ssin_validtator()
+rems_event()
