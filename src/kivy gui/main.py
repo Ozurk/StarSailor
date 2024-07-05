b93939c2c2069d424d18e2810a171a61f4d2bdab
Hunter Metzger <hunter2000metzger@gmail.com>
2024-07-05 08:31:57 -0400

    added a kivy directory

---
+++ src/kivy gui/main.py
@@ -0,0 +1,18 @@
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.image import Image
+from kivy.app import App
+
+
+class Starsailor(GridLayout):
+    pass
+
+
+class StarsailorApp(App):
+    def build(self):
+        return Starsailor()
+
+
+if __name__ == '__main__':
+    StarsailorApp().run()
