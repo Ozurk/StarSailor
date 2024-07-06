587f1ef8bc9e03b9262b0ea74f6a3ec302899c3b
Ozurk <157186017+Ozurk@users.noreply.github.com>
2024-07-05 20:41:30 -0400

    WORKED ON KIVY GUI

--- src/kivy gui/main.py
+++ src/kivy gui/main.py
@@ -4,16 +4,45 @@ from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.graphics import Color, Rectangle
 from kivy.app import App
-#:import utils kivy.utils
-
-
-class Starsailor(GridLayout):
-    pass
+from kivy.properties import StringProperty
+from kivy.uix.widget import Widget
+from kivy.properties import DictProperty
+from kivy.properties import NumericProperty
+from kivy.uix.button import ButtonBehavior
 
 
 class StarsailorApp(App):
     def build(self):
         return Starsailor()
+
+
+class Starsailor(GridLayout):
+    def create_new_game(self):
+        NewGameScreen()
+        Player.food = 100
+        Player.money = 1000
+        Player.sanity = 85
+
+
+
+class PersistentFrame(GridLayout):
+    pass
+
+class NewGameScreen(BoxLayout):
+    pass
+
+
+
+
+
+class Player(Widget):
+    name = StringProperty
+    food = NumericProperty
+    money = NumericProperty
+    sanity = NumericProperty
+
+
+
 
 
 if __name__ == '__main__':
