288882793a47a01e4bf4585c25ebfffcfdcc2b9e
Hunter Metzger <hunter2000metzger@gmail.com>
2024-07-08 19:33:37 -0400

    Worked on GUI a little bit

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
+from kivy.uix.textinput import TextInput
 from kivy.graphics import Color, Rectangle
 from kivy.app import App
 from kivy.properties import StringProperty
@@ -14,25 +15,31 @@ from kivy.lang import Builder
 
 Builder.load_file("Starsailor.kv")
 
+
 class StarsailorApp(App):
     def build(self):
         return Starsailor()
 
 
 class Starsailor(GridLayout):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.main_frame = self.ids.ActiveFrame
+        self.bottom_tool_bar = self.ids.Bottom_Tool_Bar
+
+    def clear_active_frame(self):
+        self.bottom_tool_bar.clear_widgets()
+        self.main_frame.clear_widgets()
+
     def create_new_game(self):
-        NewGameScreen()
         Player.food = 100
         Player.money = 1000
         Player.sanity = 85
-
-
-
-class PersistentFrame(GridLayout):
-    pass
-
-class NewGameScreen(BoxLayout):
-    pass
+        self.clear_active_frame()
+        text_label = Label(text="Name")
+        text_entry0 = TextInput()
+        self.add_widget(text_label)
+        self.add_widget(text_entry0)
 
 
 
@@ -43,9 +50,6 @@ class Player(Widget):
     food = NumericProperty
     money = NumericProperty
     sanity = NumericProperty
-
-
-
 
 
 if __name__ == '__main__':
