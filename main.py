2b4cdd714ea79fd61dfa0faa8a0744ee97de0b96
Hunter Metzger <hunter2000metzger@gmail.com>
2024-07-20 23:15:45 -0400

    Worked on starsailor

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ from kivy.lang import Builder
 from kivy.vector import Vector
 from kivy.animation import Animation
 from kivy.uix.widget import Widget
+from kivy.uix.scrollview import ScrollView
 Builder.load_file("Starsailor.kv")
 
 
@@ -32,7 +33,6 @@ class Boat(Image):
         animation = Animation(height=_height, width=_width, duration=_duration)
         animation.start(self)
         
-
 
 class Planet(Widget):
     def move_to(self, x_coord, y_coord, _duration):
@@ -47,70 +47,42 @@ class Planet(Widget):
 
 
 
-
 class StarsailorApp(App):
     def build(self):
-        self.root_widget = Starsailor()
-        return self.root_widget
+        return  Starsailor()
     
 
 
 class Starsailor(GridLayout):
+    active_frame = self.ids.ActiveFrame
     name = StringProperty
     food = NumericProperty
     money = NumericProperty
     sanity = NumericProperty
     inventory: ListProperty
-    text_entry0 = None
-
 
 
     def clear_active_frame(self, *args):
         self.ids.ActiveFrame.clear_widgets()
         self.ids.Bottom_Tool_Bar.clear_widgets()
 
-    def create_new_game(self):
+    def create_new_game(self, *args):
         self.food = 100
         self.money = 1000
         self.sanity = 85
         self.clear_active_frame()
-
-        text_label = Label(text="Name")
-        self.text_entry0 = TextInput(font_size=120)
-        self.ids.ActiveFrame.add_widget(text_label)
-        self.ids.ActiveFrame.add_widget(self.text_entry0)
-
-        button = Button(text="Save", background_color=(1, 0, 0, 1), on_press=self.save_name_and_clear(),
-                        on_release=self.create_intro_screen)
-
-        self.ids.ActiveFrame.add_widget(button)
-
-    def save_name_and_clear(self, *args):
-        self.name = self.text_entry0.text
-        self.clear_active_frame()
-
-    def create_intro_screen(self, *args):
-        self.ids.ActiveFrame.add_widget(IntroScreen())
-
-    def proceed_to_heavens_forge(self, *args):
-        active_frame = self.ids['ActiveFrame']
-        active_frame.clear_widgets()
-        active_frame.add_widget(HeavensForge())
-        
+        active_frame.add_widget(MainGameWindow())
 
 
-class IntroScreen(BoxLayout):
+
+
+class MainGameWindow(ScrollView):
     pass
 
+      
 
-class HeavensForge(FloatLayout):
-    def intro_animation(self):
-        boat = self.ids.ship
-        planet = self.ids.planetid
-        boat.move_to(planet.x,planet.y, 5)
-        boat.change_size(.25, .25, 5)
-
-
+class Boat(Image):
+    pass
 
 if __name__ == '__main__':
     StarsailorApp().run()
