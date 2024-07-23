e2a0992ed58a86c0163639b511536d213aa2578a
Hunter Metzger <hunter2000metzger@gmail.com>
2024-07-22 20:42:12 -0400

    Next time I need to get the scrolling figured out.

--- .buildozer/android/app/main.py
+++ .buildozer/android/app/main.py
@@ -5,12 +5,9 @@ from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
 from kivy.graphics import Color, Rectangle
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty, ListProperty, ObjectProperty
 from kivy.uix.widget import Widget
-from kivy.properties import DictProperty
-from kivy.properties import NumericProperty
-from kivy.uix.button import ButtonBehavior, Button
-from kivy.properties import ListProperty
+from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.scatter import Scatter
 from kivy.core.window import Window
@@ -18,9 +15,8 @@ from kivy.clock import Clock
 from kivy.animation import Animation
 from kivy.lang import Builder
 from kivy.vector import Vector
-from kivy.animation import Animation
-from kivy.uix.widget import Widget
 from kivy.uix.scrollview import ScrollView
+
 Builder.load_file("Starsailor.kv")
 
 
@@ -39,33 +35,33 @@ class Planet(Widget):
         animation = Animation(x=x_coord, y=y_coord, duration=_duration)
         animation.start(self)
 
-    def change_size(self, size: list, _duration):
-        
+    def change_size(self, size, _duration):
         animation = Animation(size=size, duration=_duration)
         animation.start(self)
 
 
-
-
-class StarsailorApp(App):
-    def build(self):
-        main_widget = Starsailor()
-        return  main_widget
+class MainGameWindow(ScrollView):
+    ship = ObjectProperty(None)
     
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            parent = self.ship.parent
+            touch_pos = parent.to_widget(*touch.pos)
+            self.ship.move_to(touch_pos[0], touch_pos[1], 1)
+            return True
+        return super().on_touch_down(touch)
 
 
 class Starsailor(GridLayout):
-    name = StringProperty
-    food = NumericProperty
-    money = NumericProperty
-    sanity = NumericProperty
-    inventory: ListProperty
-
+    name = StringProperty()
+    food = NumericProperty(0)
+    money = NumericProperty(0)
+    sanity = NumericProperty(0)
+    inventory = ListProperty([])
 
     def clear_active_frame(self, *args):
         self.ids.ActiveFrame.clear_widgets()
         self.ids.Bottom_Tool_Bar.clear_widgets()
-
 
     def create_new_game(self, *args):
         active_frame = self.ids.ActiveFrame
@@ -77,20 +73,9 @@ class Starsailor(GridLayout):
         active_frame.add_widget(main_window)
 
 
-
-
-
-class MainGameWindow(ScrollView):
-    def on_touch_down(self, touch):
-        ship = self.ids.ship
-        ship.move_to(touch.pos[0] - (ship.width / 2), (touch.pos[1] - ship.height * 1.25), 1)
-        return super().on_touch_down(touch)
-    
-    
-
-      
-
-
+class StarsailorApp(App):
+    def build(self):
+        return Starsailor()
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -5,12 +5,9 @@ from kivy.uix.image import Image
 from kivy.uix.textinput import TextInput
 from kivy.graphics import Color, Rectangle
 from kivy.app import App
-from kivy.properties import StringProperty
+from kivy.properties import StringProperty, NumericProperty, ListProperty, ObjectProperty
 from kivy.uix.widget import Widget
-from kivy.properties import DictProperty
-from kivy.properties import NumericProperty
-from kivy.uix.button import ButtonBehavior, Button
-from kivy.properties import ListProperty
+from kivy.uix.button import Button
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.scatter import Scatter
 from kivy.core.window import Window
@@ -18,11 +15,22 @@ from kivy.clock import Clock
 from kivy.animation import Animation
 from kivy.lang import Builder
 from kivy.vector import Vector
-from kivy.animation import Animation
-from kivy.uix.widget import Widget
 from kivy.uix.scrollview import ScrollView
+
 Builder.load_file("Starsailor.kv")
 
+class MoveButtons(BoxLayout):
+
+    def scroll_up(self):
+        MainGameWindow.scroll_up(MainGameWindow)
+
+    def scroll_down(self):
+        MainGameWindow.scroll_down(MainGameWindow)
+        
+
+
+class MainMenuTopButtons(BoxLayout):
+    pass
 
 class Boat(Image):
     def move_to(self, x_coord, y_coord, _duration):
@@ -39,33 +47,40 @@ class Planet(Widget):
         animation = Animation(x=x_coord, y=y_coord, duration=_duration)
         animation.start(self)
 
-    def change_size(self, size: list, _duration):
-        
+    def change_size(self, size, _duration):
         animation = Animation(size=size, duration=_duration)
         animation.start(self)
 
 
-
-
-class StarsailorApp(App):
-    def build(self):
-        main_widget = Starsailor()
-        return  main_widget
+class MainGameWindow(ScrollView):
+    ship = ObjectProperty(None)
     
+    def on_touch_down(self, touch):
+        if self.collide_point(*touch.pos):
+            parent = self.ship.parent
+            touch_pos = parent.to_widget(*touch.pos)
+            self.ship.move_to(touch_pos[0], touch_pos[1], 1)
+            return True
+        return super().on_touch_down(touch)
+    
+    def scroll_up(self):
+        self.scroll_y = min(self.scroll_y + 0.1, 1)
+
+    def scroll_down(self):
+        self.scroll_y = max(self.scroll_y - 0.1, 0)
 
 
 class Starsailor(GridLayout):
-    name = StringProperty
-    food = NumericProperty
-    money = NumericProperty
-    sanity = NumericProperty
-    inventory: ListProperty
-
+    name = StringProperty()
+    food = NumericProperty(0)
+    money = NumericProperty(0)
+    sanity = NumericProperty(0)
+    inventory = ListProperty([])
 
     def clear_active_frame(self, *args):
         self.ids.ActiveFrame.clear_widgets()
         self.ids.Bottom_Tool_Bar.clear_widgets()
-
+        self.ids.Bottom_Tool_Bar.add_widget(MoveButtons())
 
     def create_new_game(self, *args):
         active_frame = self.ids.ActiveFrame
@@ -77,20 +92,9 @@ class Starsailor(GridLayout):
         active_frame.add_widget(main_window)
 
 
-
-
-
-class MainGameWindow(ScrollView):
-    def on_touch_down(self, touch):
-        ship = self.ids.ship
-        ship.move_to(touch.pos[0] - (ship.width / 2), (touch.pos[1] - ship.height * 1.25), 1)
-        return super().on_touch_down(touch)
-    
-    
-
-      
-
-
+class StarsailorApp(App):
+    def build(self):
+        return Starsailor()
 
 
 if __name__ == '__main__':
