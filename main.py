407ceb82f945b24487f15f1479a4da6c3b836268
Hunter Metzger <hunter2000metzger@gmail.com>
2024-12-24 16:11:47 -0500

    Added navigator stuff

--- main.py
+++ main.py
@@ -31,12 +31,12 @@ class Starsailor(BoxLayout):
         # Toggle visibility
         if inventory.opacity == 0:  # If currently hidden
             inventory.opacity = 1
-            inventory.width = 0
+            inventory.width = self.width / 2
             inventory.disabled = False
         else:  # If currently visible
             inventory.opacity = 0
-            inventory.width = self.width / 2
-            inventory.disabled = True
+            inventory.width = 0
+    
 
 
 class MainMenu(Screen):
@@ -44,8 +44,13 @@ class MainMenu(Screen):
     pass
 
 class IntroScreen(Screen):
-    # Intro_Screen
-    pass
+    intro = """As the captain of the Starbound Skiff, a ship unlike any other—crafted from starlight and wood—you’ve always been the guardian of the seas and skies.
+But disaster struck when your loyal companion, Astra the sea turtle, was taken by the nefarious Tideforged, a shadowy entity that thrives in the darkest depths.
+With Astra gone, the balance of the ocean is unraveling. Whispers from the waves reveal that Astra is being held captive in a hidden underwater stronghold,
+guarded by riddles, traps, and creatures born of the Tideforged’s wickedness.
+Your mission is clear: navigate treacherous waters, decode ancient maps, and harness the magic of the seas to find Astra.
+ But reaching her won’t be enough. To save your friend, you must face the Tideforged and restore harmony to the waters.
+Will your courage and wit be enough to save Astra and uncover the secrets of the ocean’s depths? The tides wait for no one, Captain."""
 
 class Map(Screen):
     def __init__(self, **kwargs):
@@ -112,11 +117,20 @@ class Factory(Screen):
 
 class LightWelder(Screen):
     
-    dialog = """A Overclocked Lightwelder runs red hot steam whistling off its rotund metallic frame. It's voice box
-            drones out to you.I have overseen this forge for millennia and time has taken its toll on me.
-            These rusty joints once shined with a coat of star paint but has since peeled off over the many centuries in this immense heat, 
-            and without its protective properties, I fear for my imminent dissolution. I fear the last can of starpaint was used up decades ago, should you find some. I will bestow upon 
-            you a pallet of hardlight. This will surely be invaluable in the exploration of more distant skies."""
+    dialog = """
+A Overclocked Lightwelder runs red hot steam whistling off its rotund metallic frame.
+
+It's voice box drones out to you:
+
+I have overseen this forge for millennia and time has taken its toll on me.
+
+These rusty joints once shined with a coat of star paint but has since peeled off over the many centuries in this immense heat, 
+
+and without its protective properties, I fear for my imminent dissolution.
+
+I fear the last can of starpaint was used up decades ago, should you find some. I will bestow upon you a pallet of hardlight.
+
+---This will surely be invaluable in the exploration of more distant skies---"""
 
     success = "Thank you very much. Here is your hardlight"
 
@@ -143,7 +157,25 @@ class LightWelder(Screen):
 
 
 class ChtakLanding(Screen):
-    pass
+    
+    dialog = """
+You have arrived at Ch'tak
+------------------------------------------------------------------------------------------------------------------------
+Ch'tak is a frigid wetland of icefloes and fetid swamps.
+The mire teems with flora and fauna that will devour any ill equipped expedition.
+The mud and frostbite form a grim synergy, as the former has claimed as many boots as the latter has feet.
+Fruits unique to the Deep Bog pollinate the air with thick spores that cling to the insides of ones' lungs and slowly suffocate you as they grow into barbed hairy tufts.
+Exotic parasites attracted to both heat and light swarm the air and waters, their buzzing perennial as the stench of decay pervading the muck.
+Anything that could survive here would certainly be robust, and probably a predatory.
+So imagine your surprise when you find a hospitable settlement marked in the ship's database of Imperial starcharts.
+It appears to be an abandoned outpost of the Empire, now colonized by an indigenous tribe of plant-based lifeforms.
+The rusty military base as been reclaimed by nature, and its new inhabitants have crafted a treetop village out of its overgrown skeleton.
+Gourdlike yurts hang from the canopy, intrinsically linked by a bridge network as numerous and complex as a hyphae system.
+You dock upon a weathered landing pad, next to a parapet that has been repurposed into a garden.
+You are greeted by a being with the appearance of a dandelion on two legs woven out of reeds. It shakes its head, and makes a number of obscure gestures you cannot interpret.
+It knows no language you can understand, let alone a mouth to speak with, but it clearly means you no harm and soon waddles off.
+You call after it but it ignores you completely.
+As you enter into the village, you have similar luck with all you come across."""
 
 class TwilightIslesLanding(Screen):
     pass
@@ -190,7 +222,18 @@ class Experience(Screen):
         if compass:
             compass.x_coords = boat.x
             compass.y_coords = boat.y
+        
+    dialog = """"An amber mushroom man lounges, eight feet tall even in a sitting position,
+as thick as a tree trunk, draped in finery of deepest sapphire
+and cradling a blade of shimmering Starglass between his rooted arms and legs.
+Sharp, fierce periwinkle pupils betray droopy eyelids beneath the brim his golden cap.
+The camaraderie of the court drowns out as your mind fills "
+                      "with a voice as fierce as sunlight and rich as honey:
+On a journey, it is as important to know where you are, as it is to know where you are going.
 
+Altough I can not tell you where you will find your friend,
+
+---This Universal Positioning System [UPS] informs you of where you are---"""
 
 
     
@@ -212,7 +255,41 @@ class Compass(BoxLayout):
     pass
         
 
+class Navigator(Screen):
+    def add_goggles_to_inventory(self):
+        app = App.get_running_app()
+        print(app.root.ids)
+        inventory = app.root.ids.Inventory_Grid
+        goggles = Image(source='pictures/twilight_isles/goggles.png')
+        print(inventory.ids)
+        if "Goggles" not in inventory.ids:
+            inventory.add_widget(goggles)
+            inventory.ids.Goggles = goggles
+    
+    dialog = """"Captain! A moment, if you will?"
+a chipper young woman aks you. You turn and are surprised to find yourself face to face with what appears to be a mummy.
+The whole right side of her face as well as most of her body are tightly wound in bandages, and a long lined aviator's jacket drapes easily from her shoulders.
+The left side of her face reveals a friendly smile of charming, crooked teeth and a pupil that shines with silver light.
+"Listen Cap, someone round here's gonna go gossipping to you bout me anyways, so I figure ye ought to hear it from me first.
+I was a navigator on the 'Lady Jenny'.
+A damn fine navigator, for a damn fine ship. But the crew...
 
+Have you ever heard of Siren-Suns? They weave the most wondrous light and their warmth is seductive. Our captain set a
+course that neared one's orbit, and equipped us with safety gear to protect our heads and hearts as we passed. But one
+foolish sailor hadn't properly fastened his goggles, and the sun's allure influenced him instantly. Possessed, he began
+ripping goggles off the faces of his fellows, and a mutiny of sun smitten supplicants multiplied across the ship. They
+commandeered the Jenny. They flung ol' captain off the side. I tried to fight, but was pinned back by the lot of em as
+they took the helm and steered us toward oblivion.
+
+I shielded my gaze against its whispering rays, while they blinded themselves staring into that blazing beauty. The
+reverie broke when their eyes began to bubble, but by then we didn't have much longer before the rest of our selves
+followed suite. I blindly reached for the helm and veered us off course. I remember nary after that. These turtle
+priests found us. They specialize in the the treatment of star ailments. Wrapped me up good here. Rest of my boys was
+cooked, and consigned to the skies.
+
+I took these googles from the dead deck-hand, s'pose he don't need em' anymore. 
+
+-- Take them, you will need them if you insist on venturing into the negitive x coords---"""
 
 
 
