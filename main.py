5dede0078201bc4ad769a75c27246cdc95647977
Hunter Metzger <hunter2000metzger@gmail.com>
2024-12-31 15:57:12 -0500

    added requirments

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.image import Image
-from kivy.uix.textinput import TextInput
-from kivy.graphics import Color, Rectangle
 from kivy.app import App
-from kivy.properties import StringProperty, NumericProperty, ListProperty, ObjectProperty
-from kivy.uix.widget import Widget
-from kivy.uix.button import Button
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.scatter import Scatter
-from kivy.core.window import Window
-from kivy.animation import Animation
+from kivy.properties import NumericProperty
 from kivy.lang import Builder
-from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition, SwapTransition
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.relativelayout import RelativeLayout
+from kivy.uix.screenmanager import Screen
 from kivy.clock import Clock
 Builder.load_file("Starsailor.kv")
 
